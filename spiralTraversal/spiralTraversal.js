30fe08f2d7c716c213c856833a934ead11ea9dbc
Sean Conroy <conroysean@gmail.com>
2015-09-15 23:50:07 -0700

    [feat] Adds spiralTraversal.js

---
+++ spiralTraversal/spiralTraversal.js
@@ -0,0 +1,13 @@
+var spiralTraversal = function(spiral){
+
+}
+
+
+
+var testSpiral = [[1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9]];
+
+spiralTraversal(testSpiral);
+
+// [1, 2, 3, 6, 9, 8, 7, 4, 5]
